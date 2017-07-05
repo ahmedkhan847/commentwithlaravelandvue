3204d719853c35d4fd076b741eb84923fc794d13
ahmed khan <ahmed.khan@laksol.com>
2017-07-05 09:45:34 +0500

    updating route

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('comments/{pageId}', 'CommentController@index');
 Route::post('comments', 'CommentController@store');
 Route::post('comments/{commentId}/{type}', 'CommentController@update');
